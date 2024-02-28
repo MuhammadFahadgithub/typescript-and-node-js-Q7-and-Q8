a2f1b14facc7fffd21080891598d7d84bec0a735
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-02-28 14:39:51 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,13 @@
+// Task Q7 
+//Number Eight: Write addition, subtraction, multiplication, and 
+//division operations that each result in the number 8. Be sure to 
+//enclose youroperations in print statments to see the result.
+// Task Q8
+// you should create four lines that look like this .console.log(5+3)
+// your output should simply be four lines with the number 8 appearing
+// once on each line.
+console.log(5+3) //addition
+console.log(10-2) //subtraction
+console.log(4*2) //multiplication
+console.log(16/2) //division
+
